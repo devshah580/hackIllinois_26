f2f33556fee183ff0fbc4becb051ad8cf482d4ea
Dev Shah <devshah580@gmail.com>
2026-03-01 00:27:39 -0600

    Set-up endpoint for template matching

--- target_position.py
+++ target_position.py
@@ -2,9 +2,12 @@ import cv2
 import numpy as np
 import os
 from supabase import create_client, Client
+from pathlib import Path
+
 
 url: str = "https://upsqvxtvlvxyuimngxil.supabase.co"
 key: str = "sb_publishable_KSHPacBUw328_e-hVpgboA_OdLOcGhg"
+
 supabase: Client = create_client(url, key)
 
 def upload_clusters(clusters, batch_size=500):
@@ -185,6 +188,153 @@ def template_match_bathroom(input_folder):
     
 
     return matches
+
+
+def template_match_water_fountain_file(file_path):
+    threshold = 0.625
+    ticker_path = os.path.join("ticker_water", "water_fountain.png")
+
+    if not os.path.isfile(ticker_path):
+        print("Missing water fountain template")
+        return
+
+    ticker = cv2.imread(ticker_path)
+    ticker_gray = cv2.cvtColor(ticker, cv2.COLOR_BGR2GRAY)
+
+    matches = []
+
+    image = cv2.imread(file_path)
+    if image is None:
+        return
+
+    image_gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+
+
+    for scale in np.linspace(0.08, 0.15, 6):
+        resized = cv2.resize(ticker_gray, None, fx=scale, fy=scale)
+
+        if (resized.shape[0] > image_gray.shape[0] or
+            resized.shape[1] > image_gray.shape[1]):
+            continue
+
+        result = cv2.matchTemplate(
+            image_gray,
+            resized,
+            cv2.TM_CCOEFF_NORMED
+        )
+
+        locations = np.where(result >= threshold)
+
+        h, w = resized.shape
+
+        for pt in zip(locations[1], locations[0]):
+            x, y = pt
+
+            score = result[y, x]
+
+            matches.append({
+                    "image": file_path,
+                    "ticker": "water_fountain",
+                    "x": int(x + w / 2),
+                    "y": int(y),
+                    "score": float(score)
+            })
+
+    return matches
+
+
+def template_match_bathroom_file(file_path):
+    threshold = 0.625
+    ticker_folder = "ticker_bathroom"
+
+    matches = []
+
+
+    image = cv2.imread(file_path)
+    if image is None:
+        return
+
+    image_gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+
+
+    for ticker_file in os.listdir(ticker_folder):
+        ticker_path = os.path.join(ticker_folder, ticker_file)
+
+        if not os.path.isfile(ticker_path):
+            continue
+
+        ticker = cv2.imread(ticker_path)
+        if ticker is None:
+            continue
+
+        ticker_gray = cv2.cvtColor(ticker, cv2.COLOR_BGR2GRAY)
+
+        for scale in np.linspace(0.08, 0.15, 6):
+            resized = cv2.resize(
+                ticker_gray,
+                None,
+                fx=scale,
+                fy=scale
+            )
+
+            if (resized.shape[0] > image_gray.shape[0] or
+                resized.shape[1] > image_gray.shape[1]):
+                continue
+
+            result = cv2.matchTemplate(
+                image_gray,
+                resized,
+                cv2.TM_CCOEFF_NORMED
+            )
+
+            locations = np.where(result >= threshold)
+
+            h, w = resized.shape
+
+            for pt in zip(locations[1], locations[0]):
+                x, y = pt
+
+                score = result[y, x]
+
+                matches.append({
+                    "image": file_path,
+                    "ticker": ticker_file,
+                    "x": int(x + w / 2),
+                    "y": int(y),
+                    "score": float(score)
+                })
+    
+
+    return matches
+
+def template_match(input_path):
+    p = Path(input_path)
+    if p.is_file():
+        # Water fountains
+        water_matches = template_match_water_fountain_file(input_path)
+        water_clusters = cluster_matches(water_matches)
+        
+
+        # Bathrooms
+        bathroom_matches = template_match_bathroom_file(input_path)
+        bathroom_clusters = cluster_matches(bathroom_matches)
+
+        upload_clusters(water_clusters)
+        upload_clusters(bathroom_clusters)
+    elif p.is_dir():
+        # Water fountains
+        water_matches = template_match_water_fountain(input_path)
+        water_clusters = cluster_matches(water_matches)
+        
+
+        # Bathrooms
+        bathroom_matches = template_match_bathroom(input_path)
+        bathroom_clusters = cluster_matches(bathroom_matches)
+
+        upload_clusters(water_clusters)
+        upload_clusters(bathroom_clusters)
+    else:
+        print(f"'{input_path}' does not exist or is another type of file system object.")
 
 
 if __name__ == "__main__":
